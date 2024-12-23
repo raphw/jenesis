e9a5107bbee6fc107635e09ad60945002e849377
Rafael Winterhalter <rafael.wth@gmail.com>
2024-12-23 23:37:15 +0100

    Define links to prepare for self build.

---
+++ build/Main.java
@@ -0,0 +1,10 @@
+package build;
+
+import build.buildbuddy.BuildStepArgument;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! " + BuildStepArgument.class.getName());
+    }
+}
