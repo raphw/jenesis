13e10689446930c496911b1e8f98597360a50e15
Rafael Winterhalter <rafael.wth@gmail.com>
2024-12-23 23:37:11 +0100

    Add checksum status method.

--- src/main/java/build/buildbuddy/ChecksumStatus.java
+++ src/main/java/build/buildbuddy/ChecksumStatus.java
@@ -1,3 +1,33 @@
 package build.buildbuddy;
 
-public enum ChecksumStatus { ADDED, REMOVED, ALTERED, RETAINED }
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public enum ChecksumStatus {
+
+    ADDED,
+    REMOVED,
+    ALTERED,
+    RETAINED;
+
+    public static Map<Path, ChecksumStatus> diff(Map<Path, byte[]> expected, Map<Path, byte[]> actual) {
+        Map<Path, ChecksumStatus> diff = new LinkedHashMap<>();
+        Map<Path, byte[]> removed = new LinkedHashMap<>(expected);
+        for (Map.Entry<Path, byte[]> entry : actual.entrySet()) {
+            byte[] other = removed.remove(entry.getKey());
+            if (other == null) {
+                diff.put(entry.getKey(), ADDED);
+            } else if (Arrays.equals(other, entry.getValue())) {
+                diff.put(entry.getKey(), RETAINED);
+            } else {
+                diff.put(entry.getKey(), ALTERED);
+            }
+        }
+        for (Path path : removed.keySet()) {
+            diff.put(path, REMOVED);
+        }
+        return diff;
+    }
+}
