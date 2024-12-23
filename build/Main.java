a123ccec56d997311661c6175be436b366329d80
Rafael Winterhalter <rafael.wth@gmail.com>
2024-12-23 23:37:15 +0100

    Adjust main to produce first build.

--- build/Main.java
+++ build/Main.java
@@ -7,15 +7,17 @@ import build.buildbuddy.step.Jar;
 import build.buildbuddy.step.Javac;
 
 import java.nio.file.Path;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
-        BuildExecutor executor = new BuildExecutor(Path.of("live"), new HashDigestFunction("MD5"));
+        BuildExecutor executor = new BuildExecutor(Path.of("target"), new HashDigestFunction("MD5"));
         executor.addSource("sources", Path.of("sources"));
         executor.addStep("sources-bound", Bind.asSources(), "sources");
         executor.addStep("sources-javac", new Javac(), "sources-bound");
         executor.addStep("sources-jar", new Jar(), "sources-javac");
-        executor.execute(Runnable::run);
+        Map<String, Path> stages = executor.execute(Runnable::run).toCompletableFuture().join();
+        System.out.println("Built: " + stages);
     }
 }
