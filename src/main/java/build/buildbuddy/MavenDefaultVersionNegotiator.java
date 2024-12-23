d987f812382a483501c82e9959c9d5faac9325ec
Rafael Winterhalter <rafael.wth@gmail.com>
2024-12-23 23:37:14 +0100

    Add version comparison.

--- src/main/java/build/buildbuddy/MavenDefaultVersionNegotiator.java
+++ src/main/java/build/buildbuddy/MavenDefaultVersionNegotiator.java
@@ -46,15 +46,20 @@ public class MavenDefaultVersionNegotiator implements MavenVersionNegotiator {
         return switch (version) {
             case "RELEASE" -> toMetadata(groupId, artifactId).release();
             case "LATEST" -> toMetadata(groupId, artifactId).latest();
-            case String range when (range.startsWith("[") || range.startsWith("(")) && range.contains(",")-> {
-                String[] values = range.substring(1, range.length() - 2).split(",", 2);
-                toMetadata(groupId, artifactId).versions();
-                yield values[1];
-            }
-            case String fixed when fixed.startsWith("[") || fixed.startsWith("(") -> {
-                String value = fixed.substring(1, fixed.length() - 2);
-                toMetadata(groupId, artifactId).versions();
-                yield value;
+            case String range when (range.startsWith("[") || range.startsWith("(")) && (range.endsWith("]") || range.endsWith(")")) -> {
+                String value = range.substring(1, range.length() - 2), minimum, maximum;
+                int includeMinimum = range.startsWith("[") ? 0 : 1, includeMaximum = range.endsWith("]") ? 0 : 1, index = value.indexOf(',');
+                if (index == -1) {
+                    minimum = maximum = value.trim();
+                } else {
+                    minimum = value.substring(0, index).trim();
+                    maximum = value.substring(index + 1).trim();
+                }
+                yield toMetadata(groupId, artifactId).versions().stream()
+                        .filter(candidate -> compare(candidate, minimum) < includeMinimum)
+                        .filter(candidate -> compare(candidate, maximum) < includeMaximum)
+                        .reduce((left, right) -> right)
+                        .orElseThrow(() -> new IllegalStateException("Could not resolve version in range: " + version));
             }
             default -> version;
         };
@@ -111,6 +116,63 @@ public class MavenDefaultVersionNegotiator implements MavenVersionNegotiator {
             cache.put(new MavenPomResolver.DependencyName(groupId, artifactId), metadata);
         }
         return metadata;
+    }
+
+    static int compare(String left, String right) {
+        int leftIndex = 0, rightIndex = 0;
+        while (leftIndex < left.length() && rightIndex < right.length()) {
+            int leftNext = leftIndex, rightNext = rightIndex;
+            while (leftNext < left.length()) {
+                if (left.charAt(leftNext) == '.' || left.charAt(leftNext) == '-') {
+                    break;
+                } else {
+                    leftNext++;
+                }
+            }
+            while (rightNext < right.length()) {
+                if (right.charAt(rightNext) == '.' || right.charAt(rightNext) == '-') {
+                    break;
+                } else {
+                    rightNext++;
+                }
+            }
+            boolean leftText = false, rightText = false;
+            int leftInteger = 0, rightInteger = 0;
+            try {
+                leftInteger = Integer.parseInt(left, leftIndex, leftNext, 10);
+            } catch (NumberFormatException ignored) {
+                leftText = true;
+            }
+            try {
+                rightInteger = Integer.parseInt(right, rightIndex, rightNext, 10);
+            } catch (NumberFormatException ignored) {
+                rightText = true;
+            }
+            int comparison;
+            if (leftText && rightText) {
+                comparison = CharSequence.compare(
+                        left.subSequence(leftIndex, leftNext),
+                        right.substring(rightIndex, rightNext));
+            } else if (leftText) {
+                return 1;
+            } else if (rightText) {
+                return -1;
+            } else {
+                comparison = Integer.compare(leftInteger, rightInteger);
+            }
+            if (comparison != 0) {
+                return comparison;
+            }
+            leftIndex = leftNext;
+            rightIndex = rightNext;
+        }
+        if (leftIndex < left.length()) {
+            return -1;
+        } else if (rightIndex < right.length()) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     private record Metadata(String latest, String release, List<String> versions) {
