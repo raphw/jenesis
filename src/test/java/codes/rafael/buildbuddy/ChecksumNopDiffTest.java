d8f9c2fd118be409f5205fc22870e58476806aee
Rafael Winterhalter <rafael.wth@gmail.com>
2024-12-23 23:37:11 +0100

    Fix test.

--- src/test/java/codes/rafael/buildbuddy/ChecksumNopDiffTest.java
+++ src/test/java/codes/rafael/buildbuddy/ChecksumNopDiffTest.java
@@ -17,14 +17,10 @@ public class ChecksumNopDiffTest {
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-    private ChecksumNopDiff nop = new ChecksumNopDiff();
-
     @Test
     public void can_diff_empty() throws IOException {
         Path sources = temporaryFolder.newFolder("sources").toPath();
         Path first = toFile(sources, "first", "foo"), second = toFile(sources, "second", "bar");
-        Path checksums = temporaryFolder.newFile("checksums.MD5").toPath();
-        Files.delete(checksums);
         Map<Path, ChecksumStatus> traversal = ChecksumNopDiff.traverse(sources);
         assertThat(traversal).containsOnlyKeys(sources.relativize(first), sources.relativize(second));
         assertThat(traversal.get(sources.relativize(first))).isEqualTo(ChecksumStatus.ADDED);
@@ -38,5 +34,4 @@ public class ChecksumNopDiffTest {
         }
         return file;
     }
-
 }
