173343862623d8ea9a538969d7c3f763e3307e4d
Rafael Winterhalter <rafael.wth@gmail.com>
2024-12-23 23:37:13 +0100

    Add filter command.

--- src/main/java/build/buildbuddy/BuildStep.java
+++ src/main/java/build/buildbuddy/BuildStep.java
@@ -1,10 +1,12 @@
 package build.buildbuddy;
 
 import java.io.IOException;
-import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Executor;
+import java.util.stream.Stream;
 
 @FunctionalInterface
 public interface BuildStep {
@@ -16,4 +18,26 @@ public interface BuildStep {
     CompletionStage<BuildStepResult> apply(Executor executor,
                                            BuildStepContext context,
                                            Map<String, BuildStepArgument> arguments) throws IOException;
+
+    default BuildStep without(String... identities) {
+        return new BuildStep() {
+            @Override
+            public boolean isAlwaysRun() {
+                return BuildStep.this.isAlwaysRun();
+            }
+
+            @Override
+            public CompletionStage<BuildStepResult> apply(Executor executor,
+                                                          BuildStepContext context,
+                                                          Map<String, BuildStepArgument> arguments) throws IOException {
+                Map<String, BuildStepArgument> reduction = new HashMap<>(arguments);
+                Stream.of(identities).forEach(reduction.keySet()::remove);
+                return BuildStep.this.isAlwaysRun()
+                        || context.previous() == null
+                        || reduction.values().stream().anyMatch(BuildStepArgument::isChanged)
+                        ? BuildStep.this.apply(executor, context, reduction)
+                        : CompletableFuture.completedStage(new BuildStepResult(false));
+            }
+        };
+    }
 }
