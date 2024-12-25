626e5a7c05495da9a1c9562ba6c8dedded421de5
Rafael Winterhalter <rafael.wth@gmail.com>
2024-12-25 21:22:41 +0100

    Adjust build script.

--- build/Main.java
+++ build/Main.java
@@ -3,11 +3,14 @@ package build;
 import build.buildbuddy.BuildExecutor;
 import build.buildbuddy.HashDigestFunction;
 import build.buildbuddy.step.Bind;
+import build.buildbuddy.step.JUnit4;
 import build.buildbuddy.step.Jar;
 import build.buildbuddy.step.Javac;
 
 import java.nio.file.Path;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
@@ -17,7 +20,14 @@ public class Main {
         executor.addStep("sources-bound", Bind.asSources(), "sources");
         executor.addStep("sources-javac", new Javac(), "sources-bound");
         executor.addStep("sources-jar", new Jar(), "sources-javac");
-        Map<String, Path> stages = executor.execute(Runnable::run).toCompletableFuture().join();
-        System.out.println("Built: " + stages);
+        executor.addSource("test-sources", Path.of("tests"));
+        executor.addStep("test-sources-bound", Bind.asSources(), "test-sources");
+        executor.addStep("test-sources-javac", new Javac(), "sources-javac", "test-sources-bound");
+        executor.addStep("junit", new JUnit4(), "test-sources-bound", "test-sources-javac");
+        Map<String, Path> steps;
+        try (ExecutorService executorService = Executors.newCachedThreadPool()) {
+            steps = executor.execute(executorService).toCompletableFuture().join();
+        }
+        System.out.println("Built: " + steps);
     }
 }
