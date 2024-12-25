f0c37df5d01229ca0c2887d08c51535242b72712
Rafael Winterhalter <rafael.wth@gmail.com>
2024-12-25 21:32:13 +0100

    Add javac.

--- sources/build/buildbuddy/step/Javac.java
+++ sources/build/buildbuddy/step/Javac.java
@@ -4,8 +4,12 @@ import build.buildbuddy.BuildStepArgument;
 import build.buildbuddy.BuildStepContext;
 import build.buildbuddy.ProcessBuildStep;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,21 +36,48 @@ public class Javac implements ProcessBuildStep {
     public CompletionStage<ProcessBuilder> process(Executor executor,
                                                    BuildStepContext context,
                                                    Map<String, BuildStepArgument> arguments) throws IOException {
-        List<String> commands = new ArrayList<>(List.of(javac,
+        List<String> files = new ArrayList<>(), classPath = new ArrayList<>(), commands = new ArrayList<>(List.of(javac,
                 "--release", Integer.toString(Runtime.version().version().getFirst()),
                 "-d", Files.createDirectory(context.next().resolve(CLASSES)).toString()));
         for (BuildStepArgument argument : arguments.values()) {
-            Path sources = argument.folder().resolve(Bind.SOURCES);
-            Files.walkFileTree(sources, new SimpleFileVisitor<>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-                    if (file.getFileName().toString().endsWith(".java")) {
-                        commands.add(file.toString());
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-            });
+            Path sources = argument.folder().resolve(Bind.SOURCES),
+                    classes = argument.folder().resolve(CLASSES),
+                    jars = argument.folder().resolve(Jar.JARS);
+            if (Files.exists(classes)) {
+                classPath.add(classes.toString());
+            }
+            if (Files.exists(jars)) {
+                Files.walkFileTree(sources, new FileExtensionAddingVisitor(classPath, ".jar"));
+            }
+            if (Files.exists(sources)) {
+                Files.walkFileTree(sources, new FileExtensionAddingVisitor(files, ".java"));
+            }
         }
+        if (!classPath.isEmpty()) {
+            commands.add("-classpath");
+            commands.add(String.join(File.pathSeparator, classPath));
+        }
+        commands.addAll(files);
         return CompletableFuture.completedStage(new ProcessBuilder(commands));
+    }
+
+    private static class FileExtensionAddingVisitor extends SimpleFileVisitor<Path> {
+
+        private final List<String> target;
+        private final String extension;
+
+        private FileExtensionAddingVisitor(List<String> target, String extension) {
+            this.target = target;
+            this.extension = extension;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+            if (file.getFileName().toString().endsWith(extension)) {
+                target.add(file.toString());
+            }
+            return FileVisitResult.CONTINUE;
+        }
+
     }
 }
