b6421623ac058162026323bdf090938b8189bf7e
Rafael Winterhalter <rafael.wth@gmail.com>
2025-01-06 23:29:22 +0100

    Add annotation.

--- sources/build/buildbuddy/Identifier.java
+++ sources/build/buildbuddy/Identifier.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
 
+@FunctionalInterface
 public interface Identifier {
+
+    // TODO: add method that provides check for incremental build needs.
 
     Optional<Identification> identify(Path folder) throws IOException;
 }
