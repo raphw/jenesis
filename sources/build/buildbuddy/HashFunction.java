55383be52da1281bc7984708abda005b02b612a7
Rafael Winterhalter <rafael.wth@gmail.com>
2025-01-09 19:36:35 +0100

    Add additional test.

--- sources/build/buildbuddy/HashFunction.java
+++ sources/build/buildbuddy/HashFunction.java
@@ -7,7 +7,6 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.FileTime;
 import java.util.*;
 
 @FunctionalInterface
---
+++ tests/build/buildbuddy/test/module/ModularJarResolverTest.java
@@ -0,0 +1,65 @@
+package build.buildbuddy.test.module;
+
+import build.buildbuddy.RepositoryItem;
+import build.buildbuddy.module.ModularJarResolver;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.classfile.ClassFile;
+import java.lang.classfile.attribute.ModuleAttribute;
+import java.lang.classfile.attribute.ModuleRequireInfo;
+import java.lang.constant.ModuleDesc;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.SequencedMap;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ModularJarResolverTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Test
+    public void can_parse_module_info() throws IOException {
+        SequencedMap<String, String> dependencies = new ModularJarResolver(false, (_, coordinate) -> {
+            RepositoryItem item = switch (coordinate) {
+                case "root" -> () -> toJar("sample", "transitive");
+                case "transitive" -> () -> toJar("transitive", "last");
+                case "last" -> () -> toJar("last");
+                default -> null;
+            };
+            return Optional.ofNullable(item);
+        }).dependencies(Runnable::run, new LinkedHashSet<>(Set.of("root")));
+        assertThat(dependencies).containsExactly(
+                Map.entry("root", ""),
+                Map.entry("transitive", ""),
+                Map.entry("last", ""));
+    }
+
+    private static InputStream toJar(String module, String... requires) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try (JarOutputStream jarOutputStream = new JarOutputStream(outputStream)) {
+            jarOutputStream.putNextEntry(new JarEntry("module-info.class"));
+            jarOutputStream.write(ClassFile.of().buildModule(ModuleAttribute.of(
+                    ModuleDesc.of("sample"),
+                    builder -> {
+                        builder.requires(ModuleRequireInfo.of(ModuleDesc.of("java.base"), 0, null));
+                        for (String require : requires) {
+                            builder.requires(ModuleRequireInfo.of(ModuleDesc.of(require), 0, null));
+                        }
+                    })));
+            jarOutputStream.closeEntry();
+        }
+        return new ByteArrayInputStream(outputStream.toByteArray());
+    }
+}
