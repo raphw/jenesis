bbf3ed431814a1f7644f72ea964d2d5acf195905
Rafael Winterhalter <rafael.wth@gmail.com>
2025-01-14 00:22:30 +0100

    Add minimal build description.

---
+++ build/Minimal.java
@@ -0,0 +1,47 @@
+package build;
+
+import build.buildbuddy.BuildExecutor;
+import build.buildbuddy.HashDigestFunction;
+import build.buildbuddy.Repository;
+import build.buildbuddy.Resolver;
+import build.buildbuddy.maven.MavenDefaultRepository;
+import build.buildbuddy.maven.MavenDefaultVersionNegotiator;
+import build.buildbuddy.maven.MavenPomResolver;
+import build.buildbuddy.maven.MavenRepository;
+import build.buildbuddy.step.Bind;
+import build.buildbuddy.step.Download;
+import build.buildbuddy.step.JUnit4;
+import build.buildbuddy.step.Jar;
+import build.buildbuddy.step.Javac;
+import build.buildbuddy.step.Resolve;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Minimal {
+
+    public static void main(String[] args) throws IOException {
+        MavenRepository mavenRepository = new MavenDefaultRepository();
+        Map<String, Repository> repositories = Map.of("maven", mavenRepository);
+        Map<String, Resolver> resolvers = Map.of("maven", new MavenPomResolver(
+                mavenRepository,
+                MavenDefaultVersionNegotiator.maven(mavenRepository)));
+
+        BuildExecutor executor = BuildExecutor.of(Path.of("target"), new HashDigestFunction("MD5"));
+
+        executor.addSource("sources", Bind.asSources(), Path.of("sources"));
+        executor.addStep("main-javac", new Javac(), "sources");
+        executor.addStep("main-jar", new Jar(), "main-javac");
+
+        executor.addSource("test-dependencies", Bind.asDependencies("test.properties"), Path.of("dependencies"));
+        executor.addStep("test-dependencies-resolved", new Resolve(resolvers), "test-dependencies");
+        executor.addStep("test-dependencies-downloaded", new Download(repositories), "test-dependencies-resolved");
+
+        executor.addSource("test", Bind.asSources(), Path.of("tests"));
+        executor.addStep("test-javac", new Javac(), "main-jar", "test-dependencies-downloaded", "test");
+        executor.addStep("tests", new JUnit4(), "main-jar", "test-dependencies-downloaded", "test-javac");
+
+        System.out.println("Built: " + executor.execute());
+    }
+}
