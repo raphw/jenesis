7e3f58b4b26413fb67e6edfa118f18a06414d642
Rafael Winterhalter <rafael.wth@gmail.com>
2025-01-25 22:37:27 +0100

    Add step to resolve multi-project dependencies.

--- sources/build/buildbuddy/project/MultiProjectDependencies.java
+++ sources/build/buildbuddy/project/MultiProjectDependencies.java
@@ -1,11 +1,6 @@
 package build.buildbuddy.project;
 
-import build.buildbuddy.BuildExecutorModule;
-import build.buildbuddy.BuildStep;
-import build.buildbuddy.BuildStepArgument;
-import build.buildbuddy.BuildStepContext;
-import build.buildbuddy.BuildStepResult;
-import build.buildbuddy.SequencedProperties;
+import build.buildbuddy.*;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -15,22 +10,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HexFormat;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.SequencedMap;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Executor;
+import java.util.function.Predicate;
 
 public class MultiProjectDependencies implements BuildStep {
 
-    private final String algorithm, module;
+    private final String algorithm;
+    private final Predicate<String> isModule;
 
-    public MultiProjectDependencies(String algorithm, String module) {
+    public MultiProjectDependencies(String algorithm, Predicate<String> isModule) {
         this.algorithm = algorithm;
-        this.module = module;
+        this.isModule = isModule;
     }
 
     @Override
@@ -40,7 +33,7 @@ public class MultiProjectDependencies implements BuildStep {
             throws IOException {
         SequencedMap<String, String> coordinates = new LinkedHashMap<>(), dependencies = new LinkedHashMap<>();
         for (Map.Entry<String, BuildStepArgument> entry : arguments.entrySet()) {
-            if (entry.getKey().startsWith(BuildExecutorModule.PREVIOUS + MultiProjectModule.MODULE + "/" + module)) {
+            if (isModule.test(entry.getKey())) {
                 Path file = entry.getValue().folder().resolve(DEPENDENCIES);
                 if (Files.exists(file)) {
                     Properties properties = new SequencedProperties();
@@ -52,19 +45,19 @@ public class MultiProjectDependencies implements BuildStep {
                         dependencies.put(property, value);
                     });
                 }
-            } else if (entry.getKey().startsWith(BuildExecutorModule.PREVIOUS + MultiProjectModule.MODULE + "/")) {
+            } else {
                 Path file = entry.getValue().folder().resolve(COORDINATES);
                 if (Files.exists(file)) {
                     Properties properties = new SequencedProperties();
                     try (Reader reader = Files.newBufferedReader(file)) {
                         properties.load(reader);
                     }
-                    properties.stringPropertyNames().forEach(property -> {
+                    for (String property : properties.stringPropertyNames()) {
                         String value = properties.getProperty(property);
                         if (!value.isEmpty()) {
                             coordinates.put(property, value);
                         }
-                    });
+                    }
                 }
             }
         }
@@ -78,16 +71,12 @@ public class MultiProjectDependencies implements BuildStep {
         for (Map.Entry<String, String> entry : dependencies.entrySet()) {
             String candidate = coordinates.get(entry.getKey());
             String value;
-            if (candidate != null) {
-                if (candidate.isEmpty()) {
-                    try (FileChannel channel = FileChannel.open(Path.of(candidate))) {
-                        digest.update(channel.map(FileChannel.MapMode.READ_ONLY, channel.position(), channel.size()));
-                    }
-                    value = algorithm + "/" + HexFormat.of().formatHex(digest.digest());
-                    digest.reset();
-                } else {
-                    value = candidate;
+            if (candidate != null && !candidate.isEmpty()) {
+                try (FileChannel channel = FileChannel.open(Path.of(candidate))) {
+                    digest.update(channel.map(FileChannel.MapMode.READ_ONLY, channel.position(), channel.size()));
                 }
+                value = algorithm + "/" + HexFormat.of().formatHex(digest.digest());
+                digest.reset();
             } else {
                 value = entry.getValue();
             }
---
+++ tests/build/buildbuddy/test/project/MultiProjectDependenciesTest.java
@@ -0,0 +1,76 @@
+package build.buildbuddy.test.project;
+
+import build.buildbuddy.*;
+import build.buildbuddy.project.MultiProjectDependencies;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MultiProjectDependenciesTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    private Path previous, next, supplement, module, dependency;
+
+    @Before
+    public void setUp() throws Exception {
+        Path root = temporaryFolder.newFolder("root").toPath();
+        previous = root.resolve("previous");
+        next = Files.createDirectory(root.resolve("next"));
+        supplement = Files.createDirectory(root.resolve("supplement"));
+        module = Files.createDirectory(root.resolve("module"));
+        dependency = Files.createDirectory(root.resolve("dependency"));
+    }
+
+    @Test
+    public void can_assign_coordinate_target_dependencies() throws IOException, NoSuchAlgorithmException {
+        Properties dependencies = new Properties();
+        dependencies.setProperty("baz", "");
+        try (Writer writer = Files.newBufferedWriter(module.resolve(BuildStep.DEPENDENCIES))) {
+            dependencies.store(writer, null);
+        }
+        Path target = temporaryFolder.newFile("target").toPath();
+        Files.writeString(target, "qux");
+        Properties coordinates = new Properties();
+        coordinates.setProperty("baz", target.toString());
+        try (Writer writer = Files.newBufferedWriter(dependency.resolve(BuildStep.COORDINATES))) {
+            coordinates.store(writer, null);
+        }
+        BuildStepResult result = new MultiProjectDependencies("SHA256", "foo"::equals).apply(
+                        Runnable::run,
+                        new BuildStepContext(previous, next, supplement),
+                        new LinkedHashMap<>(Map.of(
+                                "foo", new BuildStepArgument(
+                                        module,
+                                        Map.of(Path.of(BuildStep.DEPENDENCIES), ChecksumStatus.ADDED)),
+                                "bar", new BuildStepArgument(
+                                        dependency,
+                                        Map.of(Path.of(BuildStep.COORDINATES), ChecksumStatus.ADDED)))))
+                .toCompletableFuture().join();
+        assertThat(result.next()).isTrue();
+        Properties properties = new Properties();
+        try (Reader reader = Files.newBufferedReader(next.resolve(BuildStep.DEPENDENCIES))) {
+            properties.load(reader);
+        }
+        assertThat(properties.stringPropertyNames()).containsExactly("baz");
+        assertThat(properties.getProperty("baz")).isEqualTo("SHA256/" + HexFormat.of().formatHex(MessageDigest
+                .getInstance("SHA256")
+                .digest("qux".getBytes())));
+    }
+}
