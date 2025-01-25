498b8325813552a4e2a5103c9f53c95de4329160
Rafael Winterhalter <rafael.wth@gmail.com>
2025-01-25 20:47:51 +0100

    Allow simpler chained resolution.

--- sources/build/buildbuddy/BuildExecutorModule.java
+++ sources/build/buildbuddy/BuildExecutorModule.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.SequencedMap;
+import java.util.function.Function;
 
 @FunctionalInterface
 public interface BuildExecutorModule {
@@ -15,4 +16,18 @@ public interface BuildExecutorModule {
     }
 
     void accept(BuildExecutor buildExecutor, SequencedMap<String, Path> inherited) throws IOException;
+
+    default BuildExecutorModule resolve(Function<String, Optional<String>> resolver) {
+        return new BuildExecutorModule() {
+            @Override
+            public Optional<String> resolve(String path) {
+                return BuildExecutorModule.this.resolve(path).flatMap(resolver);
+            }
+
+            @Override
+            public void accept(BuildExecutor buildExecutor, SequencedMap<String, Path> inherited) throws IOException {
+                BuildExecutorModule.this.accept(buildExecutor, inherited);
+            }
+        };
+    }
 }
