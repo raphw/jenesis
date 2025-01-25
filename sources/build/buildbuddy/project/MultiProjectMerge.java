e25736459425be11c392c93aac197200cb6058c8
Rafael Winterhalter <rafael.wth@gmail.com>
2025-01-25 22:02:52 +0100

    Split into two classes.

---
+++ sources/build/buildbuddy/project/MultiProjectMerge.java
@@ -0,0 +1,92 @@
+package build.buildbuddy.project;
+
+import build.buildbuddy.*;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Executor;
+
+class MultiProjectMerge implements BuildStep {
+
+    private final String algorithm, module;
+
+    MultiProjectMerge(String algorithm, String module) {
+        this.algorithm = algorithm;
+        this.module = module;
+    }
+
+    @Override
+    public CompletionStage<BuildStepResult> apply(Executor executor,
+                                                  BuildStepContext context,
+                                                  SequencedMap<String, BuildStepArgument> arguments)
+            throws IOException {
+        SequencedMap<String, String> coordinates = new LinkedHashMap<>(), dependencies = new LinkedHashMap<>();
+        for (Map.Entry<String, BuildStepArgument> entry : arguments.entrySet()) {
+            if (entry.getKey().startsWith(BuildExecutorModule.PREVIOUS + MultiProjectModule.MODULE + "/" + module)) {
+                Path file = entry.getValue().folder().resolve(DEPENDENCIES);
+                if (Files.exists(file)) {
+                    Properties properties = new SequencedProperties();
+                    try (Reader reader = Files.newBufferedReader(file)) {
+                        properties.load(reader);
+                    }
+                    properties.stringPropertyNames().forEach(property -> {
+                        String value = properties.getProperty(property);
+                        dependencies.put(property, value);
+                    });
+                }
+            } else if (entry.getKey().startsWith(BuildExecutorModule.PREVIOUS + MultiProjectModule.MODULE + "/")) {
+                Path file = entry.getValue().folder().resolve(COORDINATES);
+                if (Files.exists(file)) {
+                    Properties properties = new SequencedProperties();
+                    try (Reader reader = Files.newBufferedReader(file)) {
+                        properties.load(reader);
+                    }
+                    properties.stringPropertyNames().forEach(property -> {
+                        String value = properties.getProperty(property);
+                        if (!value.isEmpty()) {
+                            coordinates.put(property, value);
+                        }
+                    });
+                }
+            }
+        }
+        Properties properties = new SequencedProperties();
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        for (Map.Entry<String, String> entry : dependencies.entrySet()) {
+            String candidate = coordinates.get(entry.getKey());
+            String value;
+            if (candidate != null) {
+                if (candidate.isEmpty()) {
+                    try (FileChannel channel = FileChannel.open(Path.of(candidate))) {
+                        digest.update(channel.map(FileChannel.MapMode.READ_ONLY, channel.position(), channel.size()));
+                    }
+                    value = algorithm + "/" + HexFormat.of().formatHex(digest.digest());
+                    digest.reset();
+                } else {
+                    value = candidate;
+                }
+            } else {
+                value = entry.getValue();
+            }
+            properties.setProperty(entry.getKey(), value);
+        }
+        try (Writer writer = Files.newBufferedWriter(context.next().resolve(DEPENDENCIES))) {
+            properties.store(writer, null);
+        }
+        return CompletableFuture.completedStage(new BuildStepResult(true));
+    }
+}
--- sources/build/buildbuddy/project/MultiProjectModule.java
+++ sources/build/buildbuddy/project/MultiProjectModule.java
@@ -3,18 +3,10 @@ package build.buildbuddy.project;
 import build.buildbuddy.*;
 import build.buildbuddy.step.Group;
 
-import java.io.IOException;
 import java.io.Reader;
-import java.io.Writer;
-import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
-import java.util.concurrent.Executor;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -93,7 +85,7 @@ public class MultiProjectModule implements BuildExecutorModule {
                             }
                             build.addModule(entry.getKey(), (group, previous) -> {
                                 group.addStep(RESOLVE,
-                                        new DependencyCoordinateResolver(algorithm, entry.getKey()),
+                                        new MultiProjectMerge(algorithm, entry.getKey()),
                                         previous.sequencedKeySet());
                                 group.addModule(ASSEMBLE,
                                         project.module(entry.getKey(), dependencies, arguments),
@@ -110,73 +102,5 @@ public class MultiProjectModule implements BuildExecutorModule {
                     Stream.of(GROUP),
                     identified.sequencedKeySet().stream()).collect(Collectors.toCollection(LinkedHashSet::new)));
         }, IDENTIFY);
-    }
-
-    private record DependencyCoordinateResolver(String algorithm, String name) implements BuildStep {
-        @Override
-        public CompletionStage<BuildStepResult> apply(Executor executor,
-                                                      BuildStepContext context,
-                                                      SequencedMap<String, BuildStepArgument> arguments)
-                throws IOException {
-            SequencedMap<String, String> coordinates = new LinkedHashMap<>(), dependencies = new LinkedHashMap<>();
-            for (Map.Entry<String, BuildStepArgument> entry : arguments.entrySet()) {
-                if (entry.getKey().startsWith(PREVIOUS + MODULE + "/" + name)) {
-                    Path file = entry.getValue().folder().resolve(DEPENDENCIES);
-                    if (Files.exists(file)) {
-                        Properties properties = new SequencedProperties();
-                        try (Reader reader = Files.newBufferedReader(file)) {
-                            properties.load(reader);
-                        }
-                        properties.stringPropertyNames().forEach(property -> {
-                            String value = properties.getProperty(property);
-                            dependencies.put(property, value);
-                        });
-                    }
-                } else if (entry.getKey().startsWith(PREVIOUS + MODULE + "/")) {
-                    Path file = entry.getValue().folder().resolve(COORDINATES);
-                    if (Files.exists(file)) {
-                        Properties properties = new SequencedProperties();
-                        try (Reader reader = Files.newBufferedReader(file)) {
-                            properties.load(reader);
-                        }
-                        properties.stringPropertyNames().forEach(property -> {
-                            String value = properties.getProperty(property);
-                            if (!value.isEmpty()) {
-                                coordinates.put(property, value);
-                            }
-                        });
-                    }
-                }
-            }
-            Properties properties = new SequencedProperties();
-            MessageDigest digest;
-            try {
-                digest = MessageDigest.getInstance(algorithm);
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            }
-            for (Map.Entry<String, String> entry : dependencies.entrySet()) {
-                String candidate = coordinates.get(entry.getKey());
-                String value;
-                if (candidate != null) {
-                    if (candidate.isEmpty()) {
-                        try (FileChannel channel = FileChannel.open(Path.of(candidate))) {
-                            digest.update(channel.map(FileChannel.MapMode.READ_ONLY, channel.position(), channel.size()));
-                        }
-                        value = algorithm + "/" + HexFormat.of().formatHex(digest.digest());
-                        digest.reset();
-                    } else {
-                        value = candidate;
-                    }
-                } else {
-                    value = entry.getValue();
-                }
-                properties.setProperty(entry.getKey(), value);
-            }
-            try (Writer writer = Files.newBufferedWriter(context.next().resolve(DEPENDENCIES))) {
-                properties.store(writer, null);
-            }
-            return CompletableFuture.completedStage(new BuildStepResult(true));
-        }
     }
 }
