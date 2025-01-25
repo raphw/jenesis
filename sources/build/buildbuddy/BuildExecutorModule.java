b1868eeb75c6ec7e8396c80f8e608a1334da2156
Rafael Winterhalter <rafael.wth@gmail.com>
2025-01-25 21:08:57 +0100

    Remove resolver.

--- sources/build/buildbuddy/BuildExecutorModule.java
+++ sources/build/buildbuddy/BuildExecutorModule.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.SequencedMap;
-import java.util.function.Function;
 
 @FunctionalInterface
 public interface BuildExecutorModule {
@@ -16,18 +15,4 @@ public interface BuildExecutorModule {
     }
 
     void accept(BuildExecutor buildExecutor, SequencedMap<String, Path> inherited) throws IOException;
-
-    default BuildExecutorModule resolve(Function<String, Optional<String>> resolver) {
-        return new BuildExecutorModule() {
-            @Override
-            public Optional<String> resolve(String path) {
-                return BuildExecutorModule.this.resolve(path).flatMap(resolver);
-            }
-
-            @Override
-            public void accept(BuildExecutor buildExecutor, SequencedMap<String, Path> inherited) throws IOException {
-                BuildExecutorModule.this.accept(buildExecutor, inherited);
-            }
-        };
-    }
 }
