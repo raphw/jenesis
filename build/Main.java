8ced0e77d70bfecfcc1131cd83e5d275c056179a
Rafael Winterhalter <rafael.wth@gmail.com>
2025-01-30 15:08:02 +0100

    Fix main class.

--- build/Main.java
+++ build/Main.java
@@ -2,7 +2,7 @@ package build;
 
 import java.io.IOException;
 
-public class Manual {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         Manual.main(args);
