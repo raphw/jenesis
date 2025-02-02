9a5fc418a4091cd96e9e23bb7a8587bf12229e0b
Rafael Winterhalter <rafael.wth@gmail.com>
2025-02-02 12:13:37 +0100

    Fix Maven build.

--- build/Maven.java
+++ build/Maven.java
@@ -2,21 +2,12 @@ package build;
 
 import build.buildbuddy.BuildExecutor;
 import build.buildbuddy.BuildExecutorCallback;
-import build.buildbuddy.BuildExecutorModule;
 import build.buildbuddy.HashDigestFunction;
-import build.buildbuddy.Resolver;
-import build.buildbuddy.maven.*;
-import build.buildbuddy.project.DependenciesModule;
 import build.buildbuddy.project.JavaModule;
-import build.buildbuddy.project.MultiProjectDependencies;
-import build.buildbuddy.project.MultiProjectModule;
-import build.buildbuddy.project.RepositoryMultiProject;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,9 +17,13 @@ public class Maven {
         BuildExecutor root = BuildExecutor.of(Path.of("target"),
                 new HashDigestFunction("MD5"),
                 BuildExecutorCallback.printing(System.out));
-
-        root.addModule("maven", MavenProject.make(Path.of("."), "SHA256", _ -> new JavaModule()));
-
+        root.addModule("maven", MavenProject.make(Path.of("."),
+                "SHA256",
+                (_, _) -> (buildExecutor, inherited) -> buildExecutor.addModule("java",
+                        new JavaModule(),
+                        Stream.concat(Stream.of("../dependencies/artifacts"), inherited.sequencedKeySet().stream()
+                                .filter(identity -> identity.startsWith("../../../"))).collect(
+                                Collectors.toCollection(LinkedHashSet::new)))));
         root.execute();
     }
 }
