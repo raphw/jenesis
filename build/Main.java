c8614c1dcf3573a779f9afeedeb19c767c14ca8a
Rafael Winterhalter <rafael.wth@gmail.com>
2025-02-03 16:09:38 +0100

    Make Main adaptive.

--- build/Main.java
+++ build/Main.java
@@ -1,10 +1,16 @@
 package build;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Manual.main(args);
+        if (Files.exists(Path.of("pom.xml"))) {
+            Maven.main(args);
+        } else {
+            Modular.main(args);
+        }
     }
 }
