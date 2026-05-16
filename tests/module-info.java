29ccea55eb672eadb8c2e493409e09a4b2af5481
Rafael Winterhalter <rafael.wth@gmail.com>
2026-05-17 00:56:44 +0200

    Pin more versions.

--- tests/module-info.java
+++ tests/module-info.java
@@ -6,7 +6,10 @@
  * @requires org.junit.jupiter.params 5.11.3 SHA-256/0f798ebec744c4e6605fd4f2072f41a8e989e2d469e21db5aa67cf799c0b51ec
  * @requires org.junit.jupiter.engine 5.11.3 SHA-256/e62420c99f7c0d59a2159a2ef63e61877e9c80bd722c03ca8bf3bdcea050a589
  * @requires org.junit.platform.commons 1.11.3 SHA-256/be262964b0b6b48de977c61d4f931df8cf61e80e750cc3f3a0a39cdd21c1008c
+ * @requires org.junit.platform.console 1.11.4
  * @requires org.junit.platform.engine 1.11.3 SHA-256/0043f72f611664735da8dc9a308bf12ecd2236b05339351c4741edb4d8fab0da
+ * @requires org.junit.platform.launcher 1.11.4
+ * @requires org.junit.platform.reporting 1.11.4
  * @requires org.opentest4j 1.3.0 SHA-256/48e2df636cab6563ced64dcdff8abb2355627cb236ef0bf37598682ddf742f1b
  * @requires org.apiguardian.api 1.1.2 SHA-256/b509448ac506d607319f182537f0b35d71007582ec741832a1f111e5b5b70b38
  * @requires org.assertj.core 3.27.0 SHA-256/0b4d14008475fb362c2db090bc89c41b864d870216ccf8e8188fb60eb112ad68
